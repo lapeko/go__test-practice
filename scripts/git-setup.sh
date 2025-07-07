b9ee6863393806f2e041b147dfd4fccc3fc80fc1
lapeko <lapeko88@gmail.com>
2025-07-07 14:56:40 +0200

    refactor: move git hooks into .github folder

--- scripts/git-setup.sh
+++ scripts/git-setup.sh
@@ -5,5 +5,5 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 REPO_ROOT="$(realpath "$SCRIPT_DIR/..")"
 cd "$REPO_ROOT"
 
-git config core.hooksPath .githooks
-chmod +x .githooks/commit-msg
+git config core.hooksPath .github/hooks
+chmod +x .github/hooks/commit-msg
