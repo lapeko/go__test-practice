043bc3daa8f5648897c349b20ec3914550ef7b19
lapeko <lapeko88@gmail.com>
2025-07-07 23:29:07 +0200

    test: arrays pkg unit tests

---
+++ 1-unit-test-introduction/arrays/most_frequent_test.go
@@ -0,0 +1,45 @@
+package arrays
+
+import (
+    "fmt"
+    "testing"
+)
+
+type test struct {
+    name  string
+    array []int
+    want  int
+}
+
+func (t *test) addPrefix2Name(name string) {
+    t.name = fmt.Sprintf("%s%s", name, t.name)
+}
+
+var tests = []test{{
+    name:  "([1]) should return 1",
+    array: []int{1},
+    want:  1,
+}, {
+    name:  "([-1, 1, -1, 1]) should return -1",
+    array: []int{-1, 1, -1, 1},
+    want:  -1,
+}}
+
+var funcs = map[string]func([]int) int{
+    "MostFrequent":      MostFrequent,
+    "MostFrequentNaive": MostFrequentNaive,
+}
+
+func TestAllMostFrequentImplementations(t *testing.T) {
+    for fName, fn := range funcs {
+        for _, tt := range tests {
+            tt.addPrefix2Name(fName)
+            t.Run(tt.name, func(t *testing.T) {
+                got := fn(tt.array)
+                if got != tt.want {
+                    t.Errorf("%s(%v) = %d; want %d", fName, tt.array, got, tt.want)
+                }
+            })
+        }
+    }
+}
