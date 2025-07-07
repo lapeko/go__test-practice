0b323e3ad420c4cf48adc70422a275af2b11f9b4
lapeko <lapeko88@gmail.com>
2025-07-07 23:27:42 +0200

    style: use tab intends instad of spaces

--- 1-unit-test-introduction/arrays/most_frequent.go
+++ 1-unit-test-introduction/arrays/most_frequent.go
@@ -1,31 +1,31 @@
-package main
+package arrays
 
 func MostFrequentNaive(nums []int) int {
-    var keyCount [2]int
-    for left, currentNum := range nums {
-        count := 1
-        for _, rightNum := range nums[left+1:] {
-            if currentNum == rightNum {
-                count++
-            }
-        }
-        if count > keyCount[1] {
-            keyCount = [2]int{currentNum, count}
-        }
-    }
-    return keyCount[0]
+	var keyCount [2]int
+	for left, currentNum := range nums {
+		count := 1
+		for _, rightNum := range nums[left+1:] {
+			if currentNum == rightNum {
+				count++
+			}
+		}
+		if count > keyCount[1] {
+			keyCount = [2]int{currentNum, count}
+		}
+	}
+	return keyCount[0]
 }
 
 func MostFrequent(nums []int) int {
-    keyCountMap := make(map[int]int)
-    var keyCountMax [2]int
-    for _, num := range nums {
-        keyCountMap[num]++
-    }
-    for key, val := range keyCountMap {
-        if val > keyCountMax[1] {
-            keyCountMax = [2]int{key, val}
-        }
-    }
-    return keyCountMax[0]
+	keyCountMap := make(map[int]int)
+	var keyCountMax [2]int
+	for _, num := range nums {
+		keyCountMap[num]++
+	}
+	for key, val := range keyCountMap {
+		if val > keyCountMax[1] {
+			keyCountMax = [2]int{key, val}
+		}
+	}
+	return keyCountMax[0]
 }
